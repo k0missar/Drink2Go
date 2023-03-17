90a015ce1373a2747008366053e244482e3f302c
k0missar <k0missar@yandex.ru>
2023-03-17 22:42:19 +0300

    Полностью готов функционал слайдера и мобильного меню, pixel perfect меню и слайдера во всех адаптивных вариантах

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,27 @@
 /* в этот файл добавляет скрипты*/
 // Мобильное меню
 const mainMenu = document.querySelector(".navbar__main-menu");
-mainMenu.classList.add("navbar__main-menu--close");
+if (mainMenu) {
+  mainMenu.classList.add("navbar__main-menu--close");
 
-function openCloseMenu() {
-  const buttonMainMenu = document.querySelector(".navbar__button-main-menu");
-  mainMenu.classList.toggle("navbar__main-menu--close");
+  function openCloseMenu() {
+    const buttonMainMenu = document.querySelector(".navbar__button-main-menu");
+    mainMenu.classList.toggle("navbar__main-menu--close");
+    buttonMainMenu.classList.toggle("navbar__button-main-menu--close");
+  }
 }
 
 // Слайдер
 const slider = document.querySelector(".slider");
-const sliderList = slider.querySelectorAll(".slider__item");
+const sliderList = slider.querySelectorAll(".slider__item"); // Список слайдов
 
 if (sliderList.length > 0) {
+  // Проверка наличия слайдов
+  const buttonDesktopList = slider.querySelectorAll(".slider__control-button");
   const bgColor = ["#f3ebe1", "#eae6fc", "#e5e6e8"]; // Цвета под изображение n-по порядку
+  let count = 0; // Счетчик слайдеров
 
-  let count = 0;
-  function slid(event) {
+  function handlerSlider(event) {
     // Против часовой стрелки, нажата левая кнопка
     if (event.target.className === "slider__control-left") {
       if (count > 0 && count < sliderList.length) {
@@ -41,6 +46,22 @@ if (sliderList.length > 0) {
         sliderList[count].classList.toggle("slider__item--hide");
       }
     }
+    // Замена основного фона
+    slider.style.backgroundColor = bgColor[count];
+  }
+
+  // Добавил JS классы-идентификаторы с числом в конце строки
+  buttonDesktopList.forEach((item, index) =>
+    item.classList.add(`js-index-${index}`)
+  );
+
+  function handlerSliderDesktop(event) {
+    const regex = /(\d+$){1,}/g;
+    itemSlider = event.target.className.match(regex); // Забираю последнее число из класса как идентификатор для count
+
+    sliderList[count].classList.toggle("slider__item--hide");
+    sliderList[Number(itemSlider[0])].classList.toggle("slider__item--hide");
+    count = Number(itemSlider[0]);
     // Замена основного фона
     slider.style.backgroundColor = bgColor[count];
   }
